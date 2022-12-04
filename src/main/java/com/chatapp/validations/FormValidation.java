47e878f9b3301345fe0231c0c38c7a0ce5cbd716
DESKTOP-HKGEPO2\duong <dqvinh20@clc.fitus.edu.vn>
2022-12-04 21:16:54 +0700

    Temp updated

--- src/main/java/com/chatapp/validations/FormValidation.java
+++
@@ -1,9 +0,0 @@
-package com.chatapp.validations;
-
-import javafx.scene.Node;
-
-public class FormValidation {
-    public void add(Node node, INPUT_TYPE input_type){
-        System.out.println(input_type);
-    }
-}
--- src/main/java/com/chatapp/validations/INPUT_TYPE.java
+++
@@ -1,7 +0,0 @@
-package com.chatapp.validations;
-
-public enum INPUT_TYPE {
-    USERNAME,
-    PASSWORD,
-    EMAIL
-}
